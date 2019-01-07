b4aa7e04bb577cb8a6df402278de6dcb969a32d6
destin floyd <destin.b.floyd@gmail.com>
2019-01-07 16:50:46 -0500

    changing DB for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,8 @@
-// require('dotenv').config()
+require('dotenv').config()
 const mongoose = require("mongoose")
 
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-mongoose.connect("mongodb://localhost/users").then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+// mongoose.connect("mongodb://localhost/users").then(() => {
     console.log("MONGODB is now connected")
 })
 
